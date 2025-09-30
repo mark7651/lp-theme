1b9f2a6461e666a020c7b297f5abd92cb7694036
Max <37314381+mark7651@users.noreply.github.com>
2025-09-30 16:24:56 +0300

    ripple btn

--- assets/js/app.js
+++ assets/js/app.js
@@ -512,3 +512,29 @@ function addClassOnViewport() {
 		})
 	})
 }
+
+function rippleEffect(event) {
+	const btn = event.currentTarget
+
+	const rect = btn.getBoundingClientRect()
+
+	const circle = document.createElement('span')
+	const diameter = Math.max(btn.clientWidth, btn.clientHeight)
+	const radius = diameter / 2
+
+	circle.style.width = circle.style.height = `${diameter}px`
+	circle.style.left = `${event.clientX - rect.left - radius}px`
+	circle.style.top = `${event.clientY - rect.top - radius}px`
+	circle.classList.add('ripple')
+
+	const ripple = btn.getElementsByClassName('ripple')[0]
+	if (ripple) {
+		ripple.remove()
+	}
+
+	btn.appendChild(circle)
+}
+
+document.querySelectorAll('.btn, .btn-small').forEach(button => {
+	button.addEventListener('click', rippleEffect)
+})
